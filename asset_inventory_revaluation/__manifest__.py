e230049ade9b8302018a205b6bf512dd85e5aa08
abubakr ibrahim <abubakr.ib@gmail.com>
2025-04-18 22:52:36 +0200

    Add asset_inventory_revaluation/__manifest__.py 1745009548799

---
+++ asset_inventory_revaluation/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Asset Inventory and Revaluation",
+    "version": "1.0",
+    "summary": "Module to manage asset inventory and revaluation",
+    "description": "This module allows tracking of asset inventory and performing asset revaluation.",
+    "author": "BLACKBOXAI",
+    "category": "Accounting",
+    "depends": ["base", "account"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/assets_views.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
